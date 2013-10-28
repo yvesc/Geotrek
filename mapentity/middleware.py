7f9db94dae214a910b0962197c406e4f3ce553b9
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-10-28 23:15:18 +0100

    Lazy internal user, remove import side-effect

--- mapentity/middleware.py
+++ mapentity/middleware.py
@@ -12,14 +12,21 @@ logger = logging.getLogger(__name__)
 CONVERSION_SERVER_HOST = urlparse(app_settings['CONVERSION_SERVER']).hostname
 CAPTURE_SERVER_HOST = urlparse(app_settings['CAPTURE_SERVER']).hostname
 
-try:
-    User = get_user_model()
-    internal_user = User.objects.get(username=app_settings['INTERNAL_USER'])
-except User.DoesNotExist:
-    internal_user = User(username=app_settings['INTERNAL_USER'], password=settings.SECRET_KEY)
-    internal_user.is_active = False
-    internal_user.is_staff = False
-    internal_user.save()
+
+def get_internal_user():
+    if not hasattr(get_internal_user, 'instance'):
+        username = app_settings['INTERNAL_USER']
+        try:
+            User = get_user_model()
+            internal_user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            internal_user = User(username=username,
+                                 password=settings.SECRET_KEY)
+            internal_user.is_active = False
+            internal_user.is_staff = False
+            internal_user.save()
+            get_internal_user.instance = internal_user
+    return get_internal_user.instance
 
 
 class AutoLoginMiddleware(object):
@@ -37,5 +44,5 @@ class AutoLoginMiddleware(object):
             if (remoteip and remoteip in (CONVERSION_SERVER_HOST, CAPTURE_SERVER_HOST)) or \
                (remotehost and remotehost in (CONVERSION_SERVER_HOST, CAPTURE_SERVER_HOST)):
                logger.debug("Auto-login for %s/%s" % (remoteip, remotehost))
-               request.user = internal_user
+               request.user = get_internal_user()
         return None
